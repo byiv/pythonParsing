d00604c334d30a2b55539bca7067ec4c83972074
byiv <b.ivan.s85@gmail.com>
2021-09-27 07:46:42 +0300

    less1_1 add headers

--- less1/less1_1.py
+++ less1/less1_1.py
@@ -3,18 +3,10 @@ from pprint import pprint
 
 user = 'byiv'
 url = f'https://api.github.com/users/{user}/repos'
+headers = {'Accept': 'application/vnd.github.v3+json'}
 
-response = requests.get(url)
+
+response = requests.get(url, headers = headers)
 j_data = response.json()
+
 pprint(j_data)
-
-
-
-
-
-
-
-
-
-
-
